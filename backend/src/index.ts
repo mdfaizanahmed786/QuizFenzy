7ba0dfa4bd79ea9cdc95a0b343a18d849bc88304
Saqlain <riyanahmed1703@gmail.com>
2024-10-23 15:42:08 +0530

    project-init

---
+++ backend/src/index.ts
@@ -0,0 +1,37 @@
+import express from "express"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+import cookieParser from "cookie-parser"  
+import dotenv from "dotenv"
+import { Server } from "socket.io"
+import http from "node:http"
+dotenv.config() 
+
+
+const app = express()
+app.use(cors())
+app.use(helmet())
+app.use(morgan("dev"))
+app.use(cookieParser())
+
+
+const server=http.createServer(app);
+const io = new Server(server, {
+// we can pass options here
+  });
+
+
+
+
+
+server.listen(process.env.PORT || 3000,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
+
+
+
+
+
+export default app
+
